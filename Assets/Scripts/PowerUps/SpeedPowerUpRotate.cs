3e9a724ee5f9f5700edcc497c000bf04858ecbd6
Jessie Williamson <bmwilliamson4@googlemail.com>
2024-10-27 22:54:49 +0000

    power up spinnin

--- Assets/Scripts/PowerUps/SpeedPowerUpRotate.cs
+++ Assets/Scripts/PowerUps/SpeedPowerUpRotate.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class SpeedPowerUpRotate : MonoBehaviour
 {
+
+    [SerializeField] float spinSpeed = 10f;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
     void Update()
     {
         // Just added another axis to this
-        transform.rotation = Quaternion.Euler(
-            Camera.main.transform.rotation.eulerAngles.x, 
-            Camera.main.transform.rotation.eulerAngles.y, 
+        transform.Rotate(
+            0f,
+            spinSpeed*Time.deltaTime, 
             0f
         );
+        transform.position = new Vector3(transform.position.x, startPosition.y+Mathf.Sin(Time.time)*0.5f, transform.position.z);
     }
 }
